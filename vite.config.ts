05da8580e83040a04c46b95006eaf59e31716020
JulianRomana <jjulian.romana@gmail.com>
2025-04-20 12:12:09 +0900

    change app base config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "https://julianromana.github.io/find-my-kanji",
   plugins: [vue()],
 });
